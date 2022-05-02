efaec5635e824d13e17db2735abaea43272667af
mrcruz117 <mrcruz1989@gmail.com>
2022-05-02 15:10:53 -0400

    complete!

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import datetime as dt
 import random
 import smtplib
+import pandas
 from emails import my_gmail, gmail_password, my_icloud
 
-now = dt.datetime.now()
+today = dt.datetime.now()
+month_day = (today.month, today.day)
 
-weekday = now.weekday()
-if weekday == 0:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+data = pandas.read_csv("birthdays.csv")
 
-    print(quote)
+# birthdays_dict = {(birthday_month, birthday_day): data_row}
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+print(birthdays_dict)
+
+if month_day in birthdays_dict:
+    birthday_person = birthdays_dict[month_day]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
     with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as gmail_connection:
         gmail_connection.starttls()
         gmail_connection.login(my_gmail, gmail_password)
         print("logged in")
         gmail_connection.sendmail(
             from_addr=my_gmail,
-            to_addrs=my_icloud,
-            msg=f"Subject: Motivation Quote\n\n{quote}"
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{contents}"
         )
