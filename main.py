be02471886a9a2bc5f7d34e9474c0a223e87b927
mrcruz117 <mrcruz1989@gmail.com>
2022-05-02 11:48:44 -0400

    sending emails on run

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import smtplib
-from emails import test
-connection = smtplib.SMTP("smtp.gmail.com")
+from emails import my_gmail, gmail_password, my_icloud, icloud_password
 
-print(test)
+with smtplib.SMTP("smtp.gmail.com") as gmail_connection:
+    # icloud_connection = smtplib.SMTP("smtp.mail.me.com")
+
+    gmail_connection.starttls()
+    gmail_connection.login(user=my_gmail, password=gmail_password)
+    gmail_connection.sendmail(
+        from_addr=my_gmail,
+        to_addrs=my_icloud,
+        msg="Subject:Automated Email Test\n\nTest Message"
+    )
